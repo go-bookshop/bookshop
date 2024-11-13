e7ab7b5a7e42babe0b9ebbfe3eba4bd1a58d7848
obaibula <baibula.oo@gmail.com>
2024-11-13 11:32:11 +0200

    Fix race condition in tests

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -66,7 +66,7 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	go func() {
+	go func(firstName, lastName, email, tokenPlaintext string) {
 		defer func() {
 			if recoveredErr := recover(); recoveredErr != nil {
 				app.logger.Error("%v", recoveredErr)
@@ -78,17 +78,17 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 			LastName  string
 			URL       string
 		}{
-			FirstName: user.FirstName,
-			LastName:  user.LastName,
-			URL:       fmt.Sprintf("www.shouldbesomefepageforregistration.com/activate?token=%s", token.Plaintext),
+			FirstName: firstName,
+			LastName:  lastName,
+			URL:       fmt.Sprintf("www.shouldbesomefepageforregistration.com/activate?token=%s", tokenPlaintext),
 		}
 
-		err = app.mailer.Send(user.Email, mailer.UserActivationTemplateFile, activationData)
+		err = app.mailer.Send(email, mailer.UserActivationTemplateFile, activationData)
 		if err != nil {
 			app.logger.Error("failed to send email", "err", err.Error())
 			return
 		}
-	}()
+	}(user.FirstName, user.LastName, user.Email, token.Plaintext)
 
 	err = httputil.WriteJSON(w, http.StatusAccepted, user, nil)
 	if err != nil {
