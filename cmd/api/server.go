331f4e2073362b834c589b8893119d7cff2a5f89
obaibula <baibula.oo@gmail.com>
2024-11-17 16:23:51 +0200

    Minor improvements

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -5,18 +5,10 @@ import (
 	"time"
 )
 
-func (app *application) configure() http.Handler {
-	routes := app.routes()
-
-	handler := app.enableCORS(routes)
-
-	return handler
-}
-
 func (app *application) run() error {
 	srv := &http.Server{
 		Addr:         ":4000",
-		Handler:      app.configure(),
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
