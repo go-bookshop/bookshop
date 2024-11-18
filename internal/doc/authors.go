b6b344960c369c0e311fab7adacedd9f436b85c9
st.droniv22 <st.droniv22@gmail.com>
2024-11-18 20:32:27 +0200

    Fix docs response required properties

--- internal/doc/authors.go
+++ internal/doc/authors.go
@@ -10,15 +10,15 @@ func createAuthorOperation() *Operation {
 	reqSchema.AddProperty("bio", "string", "A brief biography of the author", true)
 
 	successSchema := NewSchema()
-	successSchema.AddProperty("id", "integer", "Generated id of the author", true)
-	successSchema.AddProperty("name", "string", "The name of the author", true)
-	successSchema.AddProperty("bio", "string", "A brief biography of the author", true)
+	successSchema.AddProperty("id", "integer", "Generated id of the author", false)
+	successSchema.AddProperty("name", "string", "The name of the author", false)
+	successSchema.AddProperty("bio", "string", "A brief biography of the author", false)
 
 	requestErrorSchema := NewSchema()
-	requestErrorSchema.AddProperty("errMsg", "string", "Specific error", true)
+	requestErrorSchema.AddProperty("errMsg", "string", "Specific error", false)
 
 	validationErrorSchema := NewSchema()
-	validationErrorSchema.AddProperty("errors", "object", "A map of errors with fields as keys", true)
+	validationErrorSchema.AddProperty("errors", "object", "A map of errors with fields as keys", false)
 
 	op.AddRequestSchema(reqSchema, "application/json", true)
 	op.AddResponseSchema(successSchema, "201", "application/json", "Success")
--- internal/doc/registration.go
+++ internal/doc/registration.go
@@ -10,17 +10,17 @@ func registerUserOperation() *Operation {
 	reqSchema.AddProperty("password", "string", "The Password for the user", true)
 
 	successSchema := NewSchema()
-	successSchema.AddProperty("id", "integer", "Generated id of the user", true)
-	successSchema.AddProperty("firstName", "string", "The first name of the user", true)
-	successSchema.AddProperty("lastName", "string", "The last name of the user", true)
-	successSchema.AddProperty("email", "string", "The email of the user", true)
-	successSchema.AddProperty("activated", "boolean", "Indicates whether the user has been activated via email verification", true)
+	successSchema.AddProperty("id", "integer", "Generated id of the user", false)
+	successSchema.AddProperty("firstName", "string", "The first name of the user", false)
+	successSchema.AddProperty("lastName", "string", "The last name of the user", false)
+	successSchema.AddProperty("email", "string", "The email of the user", false)
+	successSchema.AddProperty("activated", "boolean", "Indicates whether the user has been activated via email verification", false)
 
 	requestErrorSchema := NewSchema()
-	requestErrorSchema.AddProperty("errMsg", "string", "Specific error", true)
+	requestErrorSchema.AddProperty("errMsg", "string", "Specific error", false)
 
 	validationErrorSchema := NewSchema()
-	validationErrorSchema.AddProperty("errors", "object", "A map of errors with fields as keys", true)
+	validationErrorSchema.AddProperty("errors", "object", "A map of errors with fields as keys", false)
 
 	op.AddRequestSchema(reqSchema, "application/json", true)
 	op.AddResponseSchema(successSchema, "202", "application/json", "Success, sending activation email in the background")
@@ -37,17 +37,17 @@ func activateUserOperation() *Operation {
 	reqSchema.AddProperty("token", "string", "Token for user activation", true)
 
 	successSchema := NewSchema()
-	successSchema.AddProperty("id", "integer", "Generated id of the user", true)
-	successSchema.AddProperty("firstName", "string", "The first name of the user", true)
-	successSchema.AddProperty("lastName", "string", "The last name of the user", true)
-	successSchema.AddProperty("email", "string", "The email of the user", true)
-	successSchema.AddProperty("activated", "boolean", "Indicates whether the user has been activated via email verification", true)
+	successSchema.AddProperty("id", "integer", "Generated id of the user", false)
+	successSchema.AddProperty("firstName", "string", "The first name of the user", false)
+	successSchema.AddProperty("lastName", "string", "The last name of the user", false)
+	successSchema.AddProperty("email", "string", "The email of the user", false)
+	successSchema.AddProperty("activated", "boolean", "Indicates whether the user has been activated via email verification", false)
 
 	requestErrorSchema := NewSchema()
-	requestErrorSchema.AddProperty("errMsg", "string", "Specific error", true)
+	requestErrorSchema.AddProperty("errMsg", "string", "Specific error", false)
 
 	validationErrorSchema := NewSchema()
-	validationErrorSchema.AddProperty("errors", "object", "A map of errors with fields as keys", true)
+	validationErrorSchema.AddProperty("errors", "object", "A map of errors with fields as keys", false)
 
 	op.AddRequestSchema(reqSchema, "application/json", true)
 	op.AddResponseSchema(successSchema, "200", "application/json", "Success")
@@ -65,10 +65,10 @@ func resendTokenOperation() *Operation {
 	reqSchema.AddProperty("email", "string", "Registered but not activated email", true)
 
 	requestErrorSchema := NewSchema()
-	requestErrorSchema.AddProperty("errMsg", "string", "Specific error", true)
+	requestErrorSchema.AddProperty("errMsg", "string", "Specific error", false)
 
 	validationErrorSchema := NewSchema()
-	validationErrorSchema.AddProperty("errors", "object", "A map of errors with fields as keys", true)
+	validationErrorSchema.AddProperty("errors", "object", "A map of errors with fields as keys", false)
 
 	op.AddRequestSchema(reqSchema, "application/json", true)
 	op.AddResponseSchema(NoBodySchema, "202", "", "Success, sending activation email in the background")
